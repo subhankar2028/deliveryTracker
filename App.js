7b19a2f89ac5be17e977d264ad66e7b9888997ec
Subhankar Majumder <subhankar.majumder@codeclouds.co.in>
2023-01-30 02:17:53 +0530

    Updated app.js

--- App.js
+++ App.js
@@ -4,14 +4,7 @@
 // Import React
 import React from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
-import GmapsDirections from './components/GmapsDirections'
-// import MapLayout from './components/MapLayout';
-// import TrackLocation from './components/TrackLocation';
 import TestPropUpdate from './components/TestPropUpdate';
-import TestCompii from './components/TestCompii'
-import AnimatedMarkers from './components/AnimatedMarkers';
-import NavigationComp from './components/NavigationComp';
-import CompasTest from './components/CompasTest'
 
 const App = () => {
   return (
